a4a0de56e6cec16cbc65cab74be9f7eff8691b16
Brian Besaw <bjbesaw@outlook.com>
2017-10-10 12:29:42 -0400

    updated index.js with Provider code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import store from './store';
 import './reset.css';
 import './index.css';
@@ -8,23 +9,10 @@ import {toggle, newgame, bigone} from './actions';
 import Game from './components/game';
 
 
-store.dispatch(toggle());
-console.log(store.getState());
-store.dispatch(toggle());
-console.log(store.getState());
-store.dispatch(newgame());
-console.log(store.getState());
-store.dispatch(newgame());
-console.log(store.getState());
-store.dispatch(bigone(4));
-console.log(store.getState());
-store.dispatch(bigone(80));
-console.log(store.getState());
-store.dispatch(bigone(47));
-console.log(store.getState());
-
 
 ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+  <Provider store={store}>
+    <Game />
+  </Provider>,
+  document.getElementById('root')
 );
